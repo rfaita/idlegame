cfbdc3d4bba6f04a8a3a23eed95b71e6c9d90881
rfaita <rfaita@gmail.com>
2018-02-19 18:40:37 -0300

    alt

---
+++ player/src/test/java/com/idle/game/tests/config/MongoEmbeddedConfiguration.java
@@ -0,0 +1,40 @@
+package com.idle.game.tests.config;
+
+import com.github.fakemongo.Fongo;
+import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ *
+ * @author rafael
+ */
+@Configuration
+@Profile({"test"})
+@EnableMongoRepositories("com.idle.game.server.repository")
+public class MongoEmbeddedConfiguration extends AbstractMongoConfiguration {
+
+    @Value("${idle.config.player.mongodb.database}")
+    private String mongoDB;
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoDB;
+    }
+
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new Fongo(getDatabaseName()).getMongo();
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "com.idle.game.model.mongo";
+    }
+
+}
