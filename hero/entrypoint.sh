3ef2fa0b98c6ca072a537d2d3a7705e9daac45d2
rfaita <rfaita@gmail.com>
2018-02-20 09:33:30 -0300

    mehhhh

--- hero/entrypoint.sh
+++ hero/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 echo "---------------------------------OPTS------------------------------------"
 echo "JAVA_OPTS="$JAVA_OPTS
-echo "SPRING_CONFIG_NAME="$SPRING_CONFIG_NAME
+echo "SPRING_CONFIG_LOCATION="$SPRING_CONFIG_LOCATION
 echo "-------------------------------------------------------------------------"
 java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
