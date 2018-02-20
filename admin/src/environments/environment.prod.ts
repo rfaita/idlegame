906080448555a5375e04eb8748dc5ad17d36f568
rfaita <rfaita@gmail.com>
2018-02-20 10:01:22 -0300

    mehhh

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_BASE_URL: "http://51.15.130.82:9000/idle/api/",
-  AUTH_URL: "http://51.15.130.82:8180/auth",
+  API_BASE_URL: "http://51.15.222.98:9000/idle/api/",
+  AUTH_URL: "http://51.15.222.98:8180/auth",
   AUTH_REALM: "idlerealm",
   AUTH_CLIENT: "idlegame",
 };
