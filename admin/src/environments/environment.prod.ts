73d8941b355c71b2480ba958268380753a116417
rfaita <rfaita@gmail.com>
2018-02-21 11:50:37 -0300

    chat

--- admin/src/environments/environment.prod.ts
+++ admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_BASE_URL: "http://51.15.222.98:9000/idle/api/",
-  WS_CHAT_BASE_URL: "ws://51.15.222.98:9000/idle/api/chat/ws",
+  WS_CHAT_BASE_URL: "ws://51.15.222.98:8083/ws",
   AUTH_URL: "http://51.15.222.98:8180/auth",
   AUTH_REALM: "idlerealm",
   AUTH_CLIENT: "idlegame",
